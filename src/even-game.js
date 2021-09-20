fc6f9bf5103ae8be7104a9ee64aa7973c4146f60
Igor Sherepov <garryk83@mail.ru>
2021-09-20 23:30:13 +0700

    Add even-game.js

---
+++ src/even-game.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+import userName from './cli.js';
+
+const evenGame = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+};
+
+const ifNumberEven = (num) => {
+  if (num % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+let score = 0;
+
+for (let i = 0; i < 3; i += 1) {
+  const question = Math.floor(Math.random() * 100);
+  const correctAnswer = ifNumberEven(question);
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    score += 1;
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log(`Let's try again, ${userName}!`);
+    break;
+  }
+}
+
+if (score === 3) {
+  console.log(`Congratulations, ${userName}!`);
+}
+export default evenGame;
